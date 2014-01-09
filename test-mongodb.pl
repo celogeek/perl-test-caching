f6796b693f811a5571559f8b8f568cc1d0a23c1d
celogeek <me@celogeek.com>
2014-01-10 00:38:58 +0100

    add test mongodb

---
+++ test-mongodb.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Digest::MD5 qw/md5_base64/;
+use Time::HiRes qw/time/;
+use feature 'say', 'state';
+use Proc::ProcessTable;
+use MongoDB;
+use Sereal qw/encode_sereal decode_sereal/;
+
+sub get_current_process_memory {
+    state $pt = Proc::ProcessTable->new;
+    my %info = map { $_->pid => $_ } @{$pt->table};
+    return $info{$$}->rss;
+}
+
+$|=1;
+my $c = MongoDB::MongoClient->new->get_database('cache')->get_collection('cache');
+
+say "Mapping";
+my @todo = map { md5_base64($_) } (1..20_000);
+say "Starting";
+
+my $mem = get_current_process_memory();
+
+my ($read, $write, $found);
+{
+    my $s = time;
+    my $i = 0;
+    for(@todo) {
+        $i++;
+        $c->insert({k => $_, v => {md5 => $_}});
+        print "Write: $i\r" if $i % 1000 == 0;
+    }
+    $write = time - $s;
+}
+$c->ensure_index({k => 1});
+say "Write: ", scalar(@todo) / $write;
+{
+    my $s = time;
+    my $i = 0;
+    my $f = 0;
+    for(@todo) {
+        $i++;
+        my $srl = $c->find({k => $_})->next->{v};
+        $found++ if ref $srl eq 'HASH';
+        print "Read : $i\r" if $i % 1000 == 0;
+    }
+    $read = time - $s;
+}
+
+say "Read : ", scalar(@todo) / $read;
+say "Found: ", $found;
+say "Mem  : ", get_current_process_memory() - $mem;
